3cfa1ec224b84e42b63627cff170ea1c1b045de7
Sunand Raghupathi <sunraghupathi@gmail.com>
2021-06-28 11:44:19 -0400

    Init for traffic env

--- MADDPG.py
+++ MADDPG.py
@@ -10,24 +10,30 @@ from params import scale_reward
 
 
 def soft_update(target, source, t):
-    for target_param, source_param in zip(target.parameters(),
-                                          source.parameters()):
-        target_param.data.copy_(
-            (1 - t) * target_param.data + t * source_param.data)
+    for target_param, source_param in zip(target.parameters(), source.parameters()):
+        target_param.data.copy_((1 - t) * target_param.data + t * source_param.data)
 
 
 def hard_update(target, source):
-    for target_param, source_param in zip(target.parameters(),
-                                          source.parameters()):
+    for target_param, source_param in zip(target.parameters(), source.parameters()):
         target_param.data.copy_(source_param.data)
 
 
 class MADDPG:
-    def __init__(self, n_agents, dim_obs, dim_act, batch_size,
-                 capacity, episodes_before_train):
-        self.actors = [Actor(dim_obs, dim_act) for i in range(n_agents)]
-        self.critics = [Critic(n_agents, dim_obs,
-                               dim_act) for i in range(n_agents)]
+    def __init__(
+        self,
+        n_agents,
+        dim_obs,
+        dim_act,
+        batch_size,
+        capacity,
+        episodes_before_train,
+        n_units,
+    ):
+        self.actors = [Actor(dim_obs, dim_act, n_units) for i in range(n_agents)]
+        self.critics = [
+            Critic(n_agents, dim_obs, dim_act, n_units) for i in range(n_agents)
+        ]
         self.actors_target = deepcopy(self.actors)
         self.critics_target = deepcopy(self.critics)
 
@@ -43,10 +49,8 @@ class MADDPG:
         self.tau = 0.01
 
         self.var = [1.0 for i in range(n_agents)]
-        self.critic_optimizer = [Adam(x.parameters(),
-                                      lr=0.001) for x in self.critics]
-        self.actor_optimizer = [Adam(x.parameters(),
-                                     lr=0.0001) for x in self.actors]
+        self.critic_optimizer = [Adam(x.parameters(), lr=0.001) for x in self.critics]
+        self.actor_optimizer = [Adam(x.parameters(), lr=0.0001) for x in self.actors]
 
         if self.use_cuda:
             for x in self.actors:
@@ -74,16 +78,17 @@ class MADDPG:
         for agent in range(self.n_agents):
             transitions = self.memory.sample(self.batch_size)
             batch = Experience(*zip(*transitions))
-            non_final_mask = ByteTensor(list(map(lambda s: s is not None,
-                                                 batch.next_states)))
+            non_final_mask = ByteTensor(
+                list(map(lambda s: s is not None, batch.next_states))
+            )
             # state_batch: batch_size x n_agents x dim_obs
             state_batch = th.stack(batch.states).type(FloatTensor)
             action_batch = th.stack(batch.actions).type(FloatTensor)
             reward_batch = th.stack(batch.rewards).type(FloatTensor)
             # : (batch_size_non_final) x n_agents x dim_obs
             non_final_next_states = th.stack(
-                [s for s in batch.next_states
-                 if s is not None]).type(FloatTensor)
+                [s for s in batch.next_states if s is not None]
+            ).type(FloatTensor)
 
             # for current agent
             whole_state = state_batch.view(self.batch_size, -1)
@@ -92,27 +97,23 @@ class MADDPG:
             current_Q = self.critics[agent](whole_state, whole_action)
 
             non_final_next_actions = [
-                self.actors_target[i](non_final_next_states[:,
-                                                            i,
-                                                            :]) for i in range(
-                                                                self.n_agents)]
+                self.actors_target[i](non_final_next_states[:, i, :])
+                for i in range(self.n_agents)
+            ]
             non_final_next_actions = th.stack(non_final_next_actions)
-            non_final_next_actions = (
-                non_final_next_actions.transpose(0,
-                                                 1).contiguous())
+            non_final_next_actions = non_final_next_actions.transpose(0, 1).contiguous()
 
-            target_Q = th.zeros(
-                self.batch_size).type(FloatTensor)
+            target_Q = th.zeros(self.batch_size).type(FloatTensor)
 
             target_Q[non_final_mask] = self.critics_target[agent](
                 non_final_next_states.view(-1, self.n_agents * self.n_states),
-                non_final_next_actions.view(-1,
-                                            self.n_agents * self.n_actions)
+                non_final_next_actions.view(-1, self.n_agents * self.n_actions),
             ).squeeze()
             # scale_reward: to scale reward in Q functions
 
             target_Q = (target_Q.unsqueeze(1) * self.GAMMA) + (
-                reward_batch[:, agent].unsqueeze(1) * scale_reward)
+                reward_batch[:, agent].unsqueeze(1) * scale_reward
+            )
 
             loss_Q = nn.MSELoss()(current_Q, target_Q.detach())
             loss_Q.backward()
@@ -140,23 +141,20 @@ class MADDPG:
 
     def select_action(self, state_batch):
         # state_batch: n_agents x state_dim
-        actions = th.zeros(
-            self.n_agents,
-            self.n_actions)
+        actions = th.zeros(self.n_agents, self.n_actions)
         FloatTensor = th.cuda.FloatTensor if self.use_cuda else th.FloatTensor
         for i in range(self.n_agents):
             sb = state_batch[i, :].detach()
             act = self.actors[i](sb.unsqueeze(0)).squeeze()
 
-            act += th.from_numpy(
-                np.random.randn(2) * self.var[i]).type(FloatTensor)
+            act += th.from_numpy(np.random.randn(actions.shape[-1]) * self.var[i]).type(
+                FloatTensor
+            )
 
-            if self.episode_done > self.episodes_before_train and\
-               self.var[i] > 0.05:
+            if self.episode_done > self.episodes_before_train and self.var[i] > 0.05:
                 self.var[i] *= 0.999998
             act = th.clamp(act, -1.0, 1.0)
 
             actions[i, :] = act
         self.steps_done += 1
-
         return actions
--- main.py
+++ main.py
@@ -1,50 +1,51 @@
-from madrl_environments.pursuit import MAWaterWorld_mod
+from make_env import make_env
 from MADDPG import MADDPG
 import numpy as np
 import torch as th
 import visdom
 from params import scale_reward
+import time
 
 # do not render the scene
 e_render = False
 
-food_reward = 10.
-poison_reward = -1.
-encounter_reward = 0.01
-n_coop = 2
-world = MAWaterWorld_mod(n_pursuers=2, n_evaders=50,
-                         n_poison=50, obstacle_radius=0.04,
-                         food_reward=food_reward,
-                         poison_reward=poison_reward,
-                         encounter_reward=encounter_reward,
-                         n_coop=n_coop,
-                         sensor_range=0.2, obstacle_loc=None, )
+env = make_env("traffic")
 
-vis = visdom.Visdom(port=5274)
 reward_record = []
 
 np.random.seed(1234)
 th.manual_seed(1234)
-world.seed(1234)
-n_agents = world.n_pursuers
-n_states = 213
-n_actions = 2
+n_agents = env.n
+n_states = env.observation_space[0].shape[0]
+n_actions = env.action_space[0].n
 capacity = 1000000
 batch_size = 1000
+n_units = 128
 
 n_episode = 20000
-max_steps = 1000
+max_steps = 120
 episodes_before_train = 100
+save_n = 200
+n_update = 100
+total_steps = 0
 
 win = None
 param = None
 
-maddpg = MADDPG(n_agents, n_states, n_actions, batch_size, capacity,
-                episodes_before_train)
-
+maddpg = MADDPG(
+    n_agents,
+    n_states,
+    n_actions,
+    batch_size,
+    capacity,
+    episodes_before_train,
+    n_units=n_units,
+)
 FloatTensor = th.cuda.FloatTensor if maddpg.use_cuda else th.FloatTensor
+
+t_start = time.time()
 for i_episode in range(n_episode):
-    obs = world.reset()
+    obs = env.reset()
     obs = np.stack(obs)
     if isinstance(obs, np.ndarray):
         obs = th.from_numpy(obs).float()
@@ -53,10 +54,10 @@ for i_episode in range(n_episode):
     for t in range(max_steps):
         # render every 100 episodes to speed up training
         if i_episode % 100 == 0 and e_render:
-            world.render()
+            env.render()
         obs = obs.type(FloatTensor)
         action = maddpg.select_action(obs).data.cpu()
-        obs_, reward, done, _ = world.step(action.numpy())
+        obs_, reward, done, _ = env.step(action.numpy())
 
         reward = th.FloatTensor(reward).type(FloatTensor)
         obs_ = np.stack(obs_)
@@ -70,60 +71,24 @@ for i_episode in range(n_episode):
         rr += reward.cpu().numpy()
         maddpg.memory.push(obs.data, action, next_obs, reward)
         obs = next_obs
+        total_steps += 1
 
-        c_loss, a_loss = maddpg.update_policy()
-    maddpg.episode_done += 1
-    print('Episode: %d, reward = %f' % (i_episode, total_reward))
+        if total_steps % n_update == 0:
+            c_loss, a_loss = maddpg.update_policy()
+
     reward_record.append(total_reward)
+    maddpg.episode_done += 1
+
+    if i_episode % save_n == 0 and i_episode > 0:
+        avg_score = np.mean(reward_record[-save_n:])
+        print(
+            "episode",
+            i_episode,
+            "average score {:.1f}, time: {}".format(
+                avg_score, round(time.time() - t_start, 3),
+            ),
+        )
+        t_start = time.time()
 
     if maddpg.episode_done == maddpg.episodes_before_train:
-        print('training now begins...')
-        print('MADDPG on WaterWorld\n' +
-              'scale_reward=%f\n' % scale_reward +
-              'agent=%d' % n_agents +
-              ', coop=%d' % n_coop +
-              ' \nlr=0.001, 0.0001, sensor_range=0.3\n' +
-              'food=%f, poison=%f, encounter=%f' % (
-                  food_reward,
-                  poison_reward,
-                  encounter_reward))
-
-    if win is None:
-        win = vis.line(X=np.arange(i_episode, i_episode+1),
-                       Y=np.array([
-                           np.append(total_reward, rr)]),
-                       opts=dict(
-                           ylabel='Reward',
-                           xlabel='Episode',
-                           title='MADDPG on WaterWorld_mod\n' +
-                           'agent=%d' % n_agents +
-                           ', coop=%d' % n_coop +
-                           ', sensor_range=0.2\n' +
-                           'food=%f, poison=%f, encounter=%f' % (
-                               food_reward,
-                               poison_reward,
-                               encounter_reward),
-                           legend=['Total'] +
-                           ['Agent-%d' % i for i in range(n_agents)]))
-    else:
-        vis.line(X=np.array(
-            [np.array(i_episode).repeat(n_agents+1)]),
-                 Y=np.array([np.append(total_reward,
-                                       rr)]),
-                 win=win,
-                 update='append')
-    if param is None:
-        param = vis.line(X=np.arange(i_episode, i_episode+1),
-                         Y=np.array([maddpg.var[0]]),
-                         opts=dict(
-                             ylabel='Var',
-                             xlabel='Episode',
-                             title='MADDPG on WaterWorld: Exploration',
-                             legend=['Variance']))
-    else:
-        vis.line(X=np.array([i_episode]),
-                 Y=np.array([maddpg.var[0]]),
-                 win=param,
-                 update='append')
-
-world.close()
+        print("training now begins...")
